bed4558a60c5b6137b97ce1304c38fcafc46a4f2
alpona-code <mailto.alpona916@gmail.com>
2024-09-30 20:02:52 +0100

    updated version upload

--- vision.py
+++ vision.py
@@ -16,7 +16,7 @@ os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## Function to load OpenAI model and get respones
-model = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-1.5-flash')
 def get_gemini_response(input,image):
     if input!="":
        response = model.generate_content([input,image])
